3ba54f1599999e3fceaeb9ec3dfed20cf0d7d57d
Chahbaz Aman <chahbaza@gmail.com>
2023-12-19 23:23:22 +0530

    medium clock

---
+++ week-2/01-async-js/medium/2-clock.js
@@ -0,0 +1,22 @@
+/*Using `1-counter.md` or `2-counter.md` from the easy section, can you create a
+clock that shows you the current machine time?
+
+Can you make it so that it updates every second, and shows time in the following formats - 
+
+ - HH:MM::SS (Eg. 13:45:23)
+
+ - HH:MM::SS AM/PM (Eg 01:45:23 PM)
+*/
+
+const process = require('node:process');
+
+function getTimeString() {
+    return Date().match(/\d\d:\d\d:\d\d/g)[0]
+}
+
+function counter() {
+    process.stdout.write(` ${getTimeString()}\r`);
+    setTimeout(counter, 1000);
+}
+
+counter();
